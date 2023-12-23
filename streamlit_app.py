7289df534e56913e2f7a28b547ba8ec69bf76e69
Yasaswini-02 <153192701+Yasaswini-02@users.noreply.github.com>
2023-12-24 00:08:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 
 # take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
+streamlit.dataframe(fruityvice_normalized)
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
@@ -43,7 +43,7 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 #output it the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+
 
 
 
